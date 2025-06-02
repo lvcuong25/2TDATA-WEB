791a43a56760ae80b9cbe52082a5b481cc8a84da
lvcuong25 <vietcuong2532k3@gmail.com>
2025-06-02 16:50:07 +0700

    Update Header.jsx

--- FE/src/components/Header.jsx
+++ FE/src/components/Header.jsx
@@ -86,14 +86,7 @@ const Header = () => {
                     Trang chủ
                   </Link>
                 </li>
-                <li>
-                  <Link
-                    to="/about"
-                    class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Giới thiệu
-                  </Link>
-                </li>
+             
                 {isLoggedIn && (
                   <li>
                     <Link
