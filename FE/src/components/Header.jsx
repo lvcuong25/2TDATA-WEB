04c9caee037af2c21edfde5a6468dd2ea229be27
lvcuong25 <vietcuong2532k3@gmail.com>
2025-06-17 18:08:40 +0700

    Update Header.jsx

--- FE/src/components/Header.jsx
+++ FE/src/components/Header.jsx
@@ -102,19 +102,7 @@ const Header = () => {
                     Trang chủ
                   </Link>
                 </li>
-                <li>
-                  <Link
-                    to="/about"
-                    className={`block py-2 px-3 rounded-sm md:p-0 ${
-                      location.pathname === '/about'
-                        ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500'
-                        : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                    }`}
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    Giới thiệu
-                  </Link>
-                </li>
+            
                 {isLoggedIn && (
                   <li>
                     <Link
